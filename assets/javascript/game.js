86d1f984572e0f75816cd4fc9f8da4299cb6bc3b
Adam Norton <adam@adamnorton.com>
2019-09-18 08:45:56 -0500

    added variables and id's into HTML to start scripting

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var wordChoices = ["website", "gamer", "artist", "student", "football", "vehicle", "fuel", "index", "format", "front", "create", "refactor", "world", "capture", "editor", "bloated", "choice", "stylist", "photo", "changes", "function", "morning", "coffee", "array", "loop", "face", "paste", "window", "education", "beauty", "model", "desktop", "retouch", "greatest", "bottle", "dinner", "entry"];
+var secretWord = " ";
+var wins = 0;
+var loses = 0;
+var guessesLeft = 0;
+var lettersGuessed = [];
