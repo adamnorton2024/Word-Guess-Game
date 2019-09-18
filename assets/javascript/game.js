c4171984c75ced6c958dfddafda89202200ea062
Adam Norton <adam@adamnorton.com>
2019-09-18 12:49:27 -0500

    scripted picking word at random, and displaying words as underscores to mask letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,32 @@
 var wordChoices = ["website", "gamer", "artist", "student", "football", "vehicle", "fuel", "index", "format", "front", "create", "refactor", "world", "capture", "editor", "bloated", "choice", "stylist", "photo", "changes", "function", "morning", "coffee", "array", "loop", "face", "paste", "window", "education", "beauty", "model", "desktop", "retouch", "greatest", "bottle", "dinner", "entry"];
 var secretWord = " ";
 var wins = 0;
-var loses = 0;
-var guessesLeft = 0;
+var losses = 0;
+var guessesLeft;
 var lettersGuessed = [];
+
+function startGame(){
+    
+    document.onkeypress = function(keyPress) { playGame() };
+
+}
+
+function playGame(){
+    document.getElementById('message').innerHTML = " ";
+    secretWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    letterMasking();
+}
+
+function letterMasking(){
+    var maskedLetters = "";
+    for(var i=0; i < secretWord.length; i++){
+        maskedLetters += "_";
+    }
+    document.getElementById('secret-word').innerHTML = maskedLetters;
+    console.log(maskedLetters);
+    console.log(secretWord);
+
+}
+
+startGame();
+
